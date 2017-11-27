e6d7a6c63fcc137d77a33c79d2db2b6a412b3c5c
jotaeze95 <jotaeze95@gmail.com>
2017-11-27 20:17:04 -0300

    Sexto Commit

--- src/main/java/test/PruebaTDD/Bowling.java
+++ src/main/java/test/PruebaTDD/Bowling.java
@@ -16,9 +16,6 @@ public class Bowling
 	public Bowling() {
 	}
 	
-	public int getPuntaje() {
-		return puntaje;
-	}
 
 	public void Frame (int tiro1, int tiro2) {
 		this.puntaje = this.puntaje + (tiro1 + tiro2);
@@ -35,25 +32,6 @@ public class Bowling
 			System.out.println(Integer.toString(NumFrames));
 		}
 		
-	}
-	
-	/*public void JuegoTotal (int tiro1, int tiro2, int NumFrames) throws Exception{
-		if (NumFrames > 11 || tiro1 > 10 || tiro2 > 10 || tiro1 < 0 || tiro2 < 0) {
-			throw new Exception("Error");
-		}
-		else
-		{
-			for( int i=0; i < NumFrames; i++ ) {
-				int[] juego1 = new int[tiro1];
-				int[] juego2 = new int[tiro2];
-				int[] juego = new int[NumFrames][];
-				juego[i] = tiro1 + tiro2;
-			}
-				
-		}
-		
-	}*/
-		
-	
+	}	
 	
 }
